ac429bcbaaf93ce84620d4c91aa5d3f559baa9bd
João Carossine <133826422+joaopcarossine@users.noreply.github.com>
2025-03-20 19:53:20 -0300

    (3) consumo api star wars

--- api.py
+++ api.py
@@ -51,8 +51,14 @@ class API_Star_Wars(API_consumer):
         return self.__URL
 
     def extract(self, id):
-    # Atividade 4, que resultará o terceiro commit
-        pass
+        URL = self.URL + str(id)
+        try:
+            dado = requests.get(URL).json()
+            return ((dado.get('name'), dado.get('films')))
+        except:
+            pass
+
+        # Atividade 4, que resultará o terceiro commit
 
 class API_Ice_and_Fire(API_consumer):
     ''' The universe of Ice And Fire '''
